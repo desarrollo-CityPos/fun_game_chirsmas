3f6c0298be6c8f5c573e652a13af3645660db903
desarrollo-CityPos <desarrollo@citypos.com.ve>
2025-12-11 16:37:46 -0400

    Correcion en las estadisticas de la ruleta

--- src/app/services/roulette.services.ts
+++ src/app/services/roulette.services.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class RulettteService {
-  private PROBABILIDAD_ESPECIAL = 40; // 1 de 10
+  private PROBABILIDAD_ESPECIAL = 30; // 1 de 10
 
   /**
    * Determina el índice de parada final basado en la probabilidad de 1/10.
@@ -13,13 +13,17 @@ export class RulettteService {
    * @returns El índice final de parada.
    */
   public determinarIndiceParada(totalSegments: number, indiceGanadorEspecial: number): number {
+    console.log(this.PROBABILIDAD_ESPECIAL);
+
     // Genera un número aleatorio entre 1 y 10.
     const sorteo = Math.floor(Math.random() * this.PROBABILIDAD_ESPECIAL) + 1;
 
     if (sorteo === 1) {
+      this.PROBABILIDAD_ESPECIAL = 30;
       // La tirada es ganadora (1/10)
       return indiceGanadorEspecial; // Detener en el sector 0 (Premio Grande)
     } else {
+      this.PROBABILIDAD_ESPECIAL--;
       // La tirada es perdedora o de consuelo (39/10)
       let indicePerdedor: number;
       do {
