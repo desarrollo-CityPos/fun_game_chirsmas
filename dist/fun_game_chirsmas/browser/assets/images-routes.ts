2f36e97020d797c62d59936c99d4cf2a149dccdf
desarrollo-CityPos <desarrollo@citypos.com.ve>
2025-12-12 10:09:48 -0400

    Creaccion del dist

---
+++ dist/fun_game_chirsmas/browser/assets/images-routes.ts
@@ -0,0 +1,27 @@
+// Imagenes
+export const snowLandscape: string = 'assets/images/winter_snow_landscape.png';
+export const chirstmasTree: string = 'assets/images/chirstmas_tree.webp';
+export const background: string = 'assets/images/fondo.png';
+export const decorationChirstmas: string[] = [
+  'assets/images/decoracion_navideña(1).webp',
+  'assets/images/decoracion_navideña(2).webp',
+  'assets/images/decoracion_navideña(3).webp',
+  'assets/images/decoracion_navideña(4).webp',
+  'assets/images/decoracion_navideña(5).webp',
+  'assets/images/decoracion_navideña(6).webp',
+  'assets/images/decoracion_navideña(7).webp',
+];
+
+// Logotipos
+export const cityPosLogoWhiteGrey: string = 'assets/logos/citipos_logo_blanco_gris.webp';
+export const cityPosLogoWhiteGreen: string = 'assets/logos/citipos_logo_blanco_verde.png';
+export const cityPosLogoWhite: string = 'assets/logos/citipos_logo_blanco.webp';
+export const cityPosLogoGreyGreen: string = 'assets/logos/citipos_logo_gris_verde.webp';
+export const cityPosLogoBlackGreen: string = 'assets/logos/citipos_logo_negro_verde.webp';
+export const cityPosLogoBlack: string = 'assets/logos/citipos_logo_negro.webp';
+export const cityPosLogoGreyWhite: string = 'assets/logos/citipos_logo_girs_blanco.webp';
+
+// Isotipo
+export const cityPosIsotipo: string = 'assets/logos/citipos_isotipo.svg';
+
+// Otros
